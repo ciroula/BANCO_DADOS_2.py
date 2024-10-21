ced6ddaab233948ba72947b1ca6e719dd758a194
Michel <159503125+ciroula@users.noreply.github.com>
2024-10-21 14:14:11 +0000

    tem que resolver um problema

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from services.usuario_services import UsuarioService
+from repositories.usuario_repository import UsuarioRepository
+from config.connection import Session
+
+def main():
+    session = Session()
+    repository = UsuarioRepository(session)
+    service = UsuarioService(repository)
+
+    #criando um usuario.
+    service.criar_usuario("Michel2", "mic2@", "123")
+
+    #lista todos os usuarios.
+    print("\nListando todos os usuarios. ")
+    lista_usuarios = service.listar_todos_usuarios()
+    for usuario in lista_usuarios:
+        print(f"{usuario.id} - {usuario.nome} - {usuario.email} - {usuario.senha}")
+
+if __name__ == "__main__":
+    main() # Chamada para a função.
